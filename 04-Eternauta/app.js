4cdbc50702d533fb1202bb8b339a752618ec5a7f
tinchoforever <tinchoforever@gmail.com>
2016-08-17 18:14:03 -0300

    montaje devoto

--- 04-Eternauta/app.js
+++ 04-Eternauta/app.js
@@ -1,11 +1,22 @@
-var firsttrack = document.querySelector('#first');
-var firstPlayer = document.querySelector('#firstPlay');
-var playFirst = function(){
-
-	firsttrack.emit('play');
 
 
+var createPlayerFor = function(selector){
+	var playSelector = selector + 'Play';
+	var audioToPlay = document.querySelector(selector);
+	var playerAnimationTrigger = document.querySelector(playSelector);
+	var mainEventPlay = function(){
+		audioToPlay.emit('play');
+	};
+	playerAnimationTrigger.el.addEventListener('animationend',mainEventPlay);
+	playerAnimationTrigger.addEventListener('animationend',mainEventPlay);
+}
+
+
+
+
+
+var players = ['#first','#devoto'];
+for (var i = 0; i < players.length; i++) {
+	createPlayerFor(players[i]);
 };
 
-firstPlayer.el.addEventListener('animationend',playFirst);
-firstPlayer.addEventListener('animationend',playFirst);
