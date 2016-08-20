6a684648cb5f80347b6e45d9310c51cb4b069b07
tinchoforever <tinchoforever@gmail.com>
2016-08-19 23:23:26 -0300

    bug mmisterioeso

--- 04-Eternauta/app.js
+++ 04-Eternauta/app.js
@@ -2,10 +2,10 @@
 
 var createPlayerFor = function(selector){
 	var playSelector = selector + 'Play';
-	var audioToPlay = document.querySelector(selector);
-	var playerAnimationTrigger = document.querySelector(playSelector);
+	var audioToPlay = document.querySelector('#' + selector);
+	var playerAnimationTrigger = document.querySelector('#' + playSelector);
 	var mainEventPlay = function(){
-		audioToPlay.emit('play');
+		audioToPlay.emit('play' + selector);
 	};
 	playerAnimationTrigger.el.addEventListener('animationend',mainEventPlay);
 	playerAnimationTrigger.addEventListener('animationend',mainEventPlay);
@@ -15,7 +15,7 @@ var createPlayerFor = function(selector){
 
 
 
-var players = ['#first','#devoto','#bosch','#adios'];
+var players = ['first','devoto','bosch','adios'];
 for (var i = 0; i < players.length; i++) {
 	createPlayerFor(players[i]);
 };
